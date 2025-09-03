d6842e30b753faac2139e29d25f281559228c3e9
Alessandro Palumbo <mistermagicp@MacBookPro.fritz.box>
2025-09-03 12:48:39 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex w-full flex-row items-center justify-between'>
+      <h1>Neve Kickoff</h1>
+    </div>
+  );
+}
+
+export default App;
