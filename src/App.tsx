36ad41a7a42b2b6227fa86e1cc128e04a3a21948
AlessandroPalumbo97 <mr.alessandropalumbo@gmail.com>
2025-09-04 03:35:16 +0200

    installed ts packages and trasnformed every file from js to ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { type JSX } from 'react';
+
+function App(): JSX.Element {
+  return (
+    <div className='bg-accent flex h-full min-h-screen w-full flex-row items-center justify-center'>
+      <h1 className='text-accent-contrast font-davinci text-4xl'>
+        Neve Kickoff
+      </h1>
+    </div>
+  );
+}
+
+export default App;
